eb4ca45efd4513b7a4beb046ab71daba349f60e4
Lalu Prasad B <laluprasad2504@gmail.com>
2026-07-20 23:09:17 +0530

    Time: 1 ms (98.72%), Space: 47.6 MB (89.49%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] f, int n) {
+
+        if(f.length == 1 && f[0] == 0 && n == 0){
+            return true;
+        }
+         if(f.length == 1 && f[0] == 0 && n == 1){
+            return true;
+        }
+         if(f.length == 1 && f[0] == 1 && n == 0){
+            return true;
+        }
+         if(f.length == 1 && f[0] == 1 && n > 0){
+            return false;
+        }
+          if(f.length == 1 && f[0] == 0 && n > 1){
+            return false;
+        }
+        if(f.length ==2 && f[0]== 0 && f[1] == 0 && n ==1){
+            return true;
+        }
+
+        for(int i = 0; i < f.length  ; i++){
+          
+            if(i == 0 && f[0]==0 && f[1]== 0 && n> 0){
+                f[0]=1;
+                n= n-1; 
+            }else if(i == f.length-1 && f[f.length-2]==0 && f[f.length-1]== 0 && n> 0){
+                f[f.length-1]=1;
+                n= n-1;
+            }else if( i-1 >= 0 && i+1 <= f.length-1 && f[i-1] == 0 && f[i] == 0 && f[i+1]== 0 && n >0 )  {
+                f[i]=1;
+                n = n-1;
+            }
+            }
+
+            if(n == 0){
+                return true;
+                
+            }else{
+                return false;
+            }
+
+
+        }
+
+
+
+
+
+
+        
+    }
