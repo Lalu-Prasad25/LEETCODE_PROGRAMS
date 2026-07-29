abf90d303069709f256e32c6885379689956decb
Lalu Prasad B <laluprasad2504@gmail.com>
2026-07-29 11:20:55 +0530

    Time: 3 ms (58.35%), Space: 47.5 MB (11.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         int x= 0 ;
         for(Integer i :nums){
             if(h.containsKey(target-i)){
-                return new int[]{x , h.get(target-i)};
+                return new int[]{h.get(target-i),x };
             }else{
                     h.put(i,x);
                     x++;
