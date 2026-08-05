d1755c09f685b23dc5393b78ec44f44301a6abae
Lalu Prasad B <laluprasad2504@gmail.com>
2026-08-05 22:08:51 +0530

    Time: 3 ms (93.79%), Space: 46.5 MB (34.86%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        
+        if(s1.length!= t1.length){
+            return false;
+        }
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        // for(int i =0;i<= s1.length-1;i++){
+        //     if(s1[i] != t1[i]){
+        //         return false;
+        //     }
+        // }
+        s = String.valueOf(s1);
+        t = String.valueOf(t1);
+
+        return s.equals(t);
+    }
+}
