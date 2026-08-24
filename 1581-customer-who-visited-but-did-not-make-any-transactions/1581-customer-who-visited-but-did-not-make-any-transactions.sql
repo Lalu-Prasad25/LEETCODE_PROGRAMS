c897732bc71726ffa80592112294a9ee5bc2f665
Lalu Prasad B <laluprasad2504@gmail.com>
2026-08-24 18:34:29 +0530

    Time: 1347 ms (81.17%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,1 @@
+Select v. customer_id , count(v.customer_id) as count_no_trans  from Visits v left outer join Transactions t on t.visit_id = v.visit_id where t.transaction_id is null group by v.customer_id;
