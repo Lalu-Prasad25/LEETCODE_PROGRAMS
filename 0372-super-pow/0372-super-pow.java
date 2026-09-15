54e961818fc7b6345ed056a795a8848779113866
Lalu Prasad B <laluprasad2504@gmail.com>
2026-09-15 23:51:22 +0530

    Time: 4 ms (75.81%), Space: 46.1 MB (83.03%) - LeetHub

---
+++ 0372-super-pow/0372-super-pow.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int superPow(int a, int[] b) {
+        long result = 1;
+        a = a % 1337;
+        for(int i = 0; i < b.length; i++) {
+            result = power(result, 10);
+            result = (result * power(a, b[i])) % 1337;
+        }
+        return (int)result;
+    }
+    public long power(long a, int b) {
+        long result = 1;
+        for(int i = 0; i < b; i++) {
+            result = (result * a) % 1337;
+        }
+        return result;
+    }
+}
